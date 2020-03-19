ed66555d0761a97a5133ebb862cc0532046b75b4
galaydacom <galayda340@gmail.com>
2020-03-19 15:03:49 +0200

    Добавили автотест и интерактивности

--- cypress/integration/first-test/chto-to-tam.spec.js
+++ cypress/integration/first-test/chto-to-tam.spec.js
@@ -1,5 +1,8 @@
-describe('My First Test', function() {
-    it('Does not do much!', function() {
-      expect(true).to.equal(true)
+describe('Form block', function() {
+    it('should change title on button click', function() {
+      cy.visit("index.html");
+      cy.get("#pole").type("Какая-нибудь строка"); //нашли элемент через селектор, а потом через type ввели значение в поле
+      cy.get("button").click(); 
+      cy.get("h1").should("have.text", "Какая-нибудь строка");
     })
   })
